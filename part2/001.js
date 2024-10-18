4707aa91ed5429d4c6d36bf744df9c862d99ceba
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-10-18 21:41:53 +0000

    learn js part2

---
+++ part2/001.js
@@ -0,0 +1,182 @@
+// Illustration of (==) operator
+let val1 = 5;
+let val2 = '5';
+
+// Checking of operands
+console.log(val1 == 5);
+console.log(val2 == 5);        
+console.log(val1 == val1);
+
+// Check against null and boolean value
+console.log(0 == false);   
+console.log(0 == null);
+
+// Illustration of (==) operator
+let obj1 = {'val1': 'value'};
+let obj2 = {'val2': 'value'};
+
+// Checking of operands
+console.log(obj1.val1 == 'value');        
+console.log(obj1 == obj2);
+console.log(obj1.val1 == obj2.val2);
+
+// Check against undefined
+console.log(0 == undefined);   
+console.log(null == undefined);
+
+
+// Illustration of (!=) operator
+
+
+// Checking of operands
+console.log(val1 != 6);
+console.log(val2 != '5');        
+console.log(val1 != val1);
+
+// Check against null and boolean value
+console.log(0 != false);   
+console.log(0 != null);
+
+// Illustration of (!=) operator
+
+
+// Checking of operands
+console.log(obj1.val1 != 'value');        
+console.log(obj1 != obj2);
+console.log(obj1.val1 != obj2.val2);
+
+// Check against undefined
+console.log(0 != undefined);   
+console.log(null != undefined);
+
+
+// Illustration of (===) operator
+
+
+// Checking of operands
+console.log(val1 === 6);
+console.log(val2 === '5');        
+console.log(val1 === val1);
+
+// Check against null and boolean value
+console.log(0 === false);   
+console.log(0 === null);
+
+
+
+// Illustration of (===) operator
+
+// Checking of operands
+console.log(obj1.val1 === 'value');        
+console.log(obj1 === obj2);
+console.log(obj1.val1 === obj2.val2);
+
+// Check against undefined
+console.log(0 === undefined);   
+console.log(null === undefined);
+
+
+// Illustration of (!==) operator
+// let val1 = 5;
+// let val2 = '5';
+
+// Checking of operands
+console.log(val1 !== 6);
+console.log(val2 !== '5');        
+console.log(val1 !== val1);
+
+// Check against null and boolean value
+console.log(0 !== false);   
+console.log(0 !== null);
+
+// Illustration of (!==) operator
+// let obj1 = {'val1': 'value'};
+// let obj2 = {'val2': 'value'};
+
+// Checking of operands
+console.log(obj1.val1 !== 'value');        
+console.log(obj1 !== obj2);
+console.log(obj1.val1 !== obj2.val2);
+
+// Check against undefined
+console.log(0 !== undefined);   
+console.log(null !== undefined);
+
+
+// Illustration of (>) operator
+// let val1 = 5;
+// let val2 = "5";
+
+// Checking of operands
+console.log(val1 > 0);
+console.log(val2 > "10");        
+console.log(val1 > "10");
+console.log(val2 > 0);
+
+
+// Illustration of (>) operator
+// let obj1 = {'val1': 1};
+// let obj2 = {'val2': 3};
+
+// Checking of operands
+console.log(obj1.val1 > 0);        
+console.log(obj1 > obj2);
+console.log(obj1.val1 > obj2.val2);
+console.log(obj2 > obj1);
+console.log(obj2.val2 > obj1.val1);
+
+
+// Illustration of (>=) operator
+// let val1 = 5;
+// let val2 = "5";
+
+// Checking of operands
+console.log(val1 >= 5);
+console.log(val2 >= "15");        
+console.log(val1 >= "5");
+console.log(val2 >= 15);
+
+// Illustration of (>=) operator
+// let obj1 = {'val1': 1};
+// let obj2 = {'val2': 3};
+
+// Checking of operands
+console.log(obj1.val1 >= 0);        
+console.log(obj1 >= obj2);
+console.log(obj1.val1 >= obj2.val2);
+console.log(obj2 >= obj1);
+console.log(obj2.val2 >= obj1.val1);
+
+
+// Illustration of (<) operator
+// let val1 = 5;
+// let val2 = "5";
+
+// Checking of operands
+console.log(val1 < 15);
+console.log(val2 < "0");        
+console.log(val1 < "0");
+console.log(val2 < 15);
+
+
+// Illustration of (<=) operator
+// let val1 = 5;
+// let val2 = "5";
+
+// Checking of operands
+console.log(val1 <= 15);
+console.log(val2 <= "0");        
+console.log(val1 <= "0");
+console.log(val2 <= 15);
+
+// Illustration of (<=) operator
+// let obj1 = {'val1': 1};
+// let obj2 = {'val2': 3};
+
+// Checking of operands
+console.log(obj1.val1 <= 10);        
+console.log(obj1 <= obj2);
+console.log(obj1.val1 <= obj2.val2);
+console.log(obj2 <= obj1);
+console.log(obj2.val2 <= obj1.val1);
+
